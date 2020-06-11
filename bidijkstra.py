f46dadd9650cf40addfc99db7d0edb9b4d123049
irina Vergunova <i_verg@outlook.com>
2020-06-11 12:08:35 +0300

    красивое все

--- bidijkstra.py
+++ bidijkstra.py
@@ -5,6 +5,21 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 color_map = []
+G = nx.DiGraph()
+G.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+G.add_weighted_edges_from([(0, 1, 10), (0, 2, 50), (1, 3, 20), (1, 4, 70),
+                               (4, 8, 30), (4, 7, 10), (3, 7, 80), (3, 5, 40),
+                               (8, 9, 40), (9, 7, 30), (2, 6, 180), (6, 5, 10)])
+def show_graph():
+    labels = {}
+    for u, v, data in G.edges(data=True):
+        labels[(u, v)] = data['weight']
+    for n in G.nodes():
+        color_map.append('violet')
+    labels = nx.get_edge_attributes(G, "weight")
+    nx.draw_networkx_edge_labels(G, pos=nx.circular_layout(G), edge_labels=labels)
+    nx.draw_circular(G, node_color=color_map, with_labels=True, edge_labels=labels)
+    plt.show()
 
 def color_change(c, number):
     counter = 0
@@ -12,20 +27,33 @@ def color_change(c, number):
         counter += 1
     color_map[counter] = c
 
-def bidir_dijkstra(G, source, target, weight='weight'):
 
-    if source == target:
+
+def show_changed_color_graph():
+    labels = nx.get_edge_attributes(G, 'weight')
+    pos = nx.circular_layout(G)
+    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=labels)
+    nx.draw_circular(G, node_color=color_map, with_labels=True, edge_labels=labels)
+    plt.show()
+    return G
+
+def bi_dij(G, source, aim, weight='weight'):
+    show_graph()
+    if source == aim:
         return (0, [source])
+    flag = False
     push = heappush
     pop = heappop
     dists = [{}, {}]
-    paths = [{source: [source]}, {target: [target]}]
+    paths = [{source: [source]}, {aim: [aim]}]
     dist_node = [[], []]
-    seen = [{source: 0}, {target: 0}]
+    seen = [{source: 0}, {aim: 0}]
     c = count()
-
+    color_change('purple',source)
+    color_change('purple', aim)
+    show_changed_color_graph()
     push(dist_node[0], (0, next(c), source))
-    push(dist_node[1], (0, next(c), target))
+    push(dist_node[1], (0, next(c), aim))
     if G.is_directed():
         neighs = [G.successors, G.predecessors]
     else:
@@ -35,10 +63,14 @@ def bidir_dijkstra(G, source, target, weight='weight'):
     while dist_node[0] and dist_node[1]:
         direction = 1 - direction
         (dist, _, v) = pop(dist_node[direction])
+        if flag is False:
+            color_change('purple',v)
+            show_changed_color_graph()
         if v in dists[direction]:
             continue
         dists[direction][v] = dist  # visited[dir][v]
         if v in dists[1 - direction]:
+            print(dists)
             return "distance: " + str(finaldist) + "\n" + "path " + str(finalpath)
 
         for w in neighs[direction](v):
@@ -52,36 +84,26 @@ def bidir_dijkstra(G, source, target, weight='weight'):
             if w in dists[direction]:
                 if vw_length < dists[direction][w]:
                     return "Contradictory paths found"
-            elif w not in seen[direction] or vw_length < seen[direction][w]:
+            elif w not in seen[direction] or vw_length < seen[direction][w]:#важно тут выбираем нужную инцид вершину
                 seen[direction][w] = vw_length
                 push(dist_node[direction], (vw_length, next(c), w))
                 paths[direction][w] = paths[direction][v] + [w]
+                if color_map[w]!='purple':
+                    color_change('r',w)
+                show_changed_color_graph()
                 if w in seen[0] and w in seen[1]:
+                    color_change('purple', w)
+                    show_changed_color_graph()
                     totaldist = seen[0][w] + seen[1][w]
                     if finalpath == [] or finaldist > totaldist:
                         finaldist = totaldist
                         revpath = paths[1][w][:]
                         revpath.reverse()
                         finalpath = paths[0][w] + revpath[1:]
-                        for v in finalpath:
-                            color_change('r', v)
-                        labels = nx.get_edge_attributes(G, "w")
-                        nx.draw_networkx_edge_labels(G, pos=nx.circular_layout(G), labels=labels)
-                        nx.draw(G, pos=nx.circular_layout(G), node_color=color_map, with_labels=True, edge_labels=True)
-                        plt.show()
+                        flag = True
+
     return "NO PATH"
 
 
-G = nx.DiGraph()
-G.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-G.add_weighted_edges_from([(0, 1, 10), (0, 2, 50), (1, 3, 20), (1, 4, 70),
-                           (4, 8, 30), (4, 7, 10), (3, 7, 80), (3, 5, 40),
-                           (8, 9, 40), (9, 7, 30), (2, 6, 180), (6, 5, 10)])
-for n in G.nodes():
-    color_map.append('violet')
-labels = nx.get_edge_attributes(G, "w")
-nx.draw_networkx_edge_labels(G, pos=nx.circular_layout(G), labels=labels)
-nx.draw_circular(G, node_color=color_map, with_labels=True)
-plt.show()
 
-print(bidir_dijkstra(G, 0, 9))
+print(bi_dij(G, 0, 9))
